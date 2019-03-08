4ba5d07aea317f8b33826fa908eccbb0d9c8d290
kuuseke1 <36753942+kuuseke1@users.noreply.github.com>
2019-03-08 17:34:41 +0200

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -4,14 +4,23 @@ import requests
 import mimetypes
 import os
 import urllib
+import time
+import math
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
+token_expiration_time = math.inf
+access_token = ""
 
 
 def get_access_token():
     """
     Gets access token using refresh token and client credentials
     """
+    global access_token
+    global token_expiration_time
+    if access_token != "" and token_expiration_time > time.time() + 100:
+        return access_token
+
     with open('credentials.json') as f:
         creds_data = json.load(f)
     client_id = creds_data['credentials']['clientId']
@@ -29,18 +38,19 @@ def get_access_token():
 
     response_data = token_response.json()
     access_token = response_data['access_token']
+    token_expiration_time = response_data['expires_in'] + time.time()
     return access_token
 
 
-def download_file(access_token, file_name, parent_folder_id):
-    search_result = search_in_folder(access_token, parent_folder_id, file_name)
+def download_file(file_name, parent_folder_id):
+    search_result = search_in_folder(parent_folder_id, file_name)
     if search_result is None:
         return "File does not exist!"
     else:
 
         url = 'https://www.googleapis.com/drive/v3/files/' + search_result + '?alt=media'
         params = {
-            'Authorization': 'Bearer ' + access_token
+            'Authorization': 'Bearer ' + get_access_token()
         }
 
         response = requests.get(url=url, headers=params)
@@ -48,12 +58,12 @@ def download_file(access_token, file_name, parent_folder_id):
         return response.text
 
 
-def upload_file(access_token, local_path, parent_folder_id):
+def upload_file(local_path, parent_folder_id):
     file_type = mimetypes.MimeTypes().guess_type(local_path)[0]
     file_to_upload = open(local_path, 'rb')
     file_name = os.path.basename(local_path)
     headers = {
-        'Authorization': 'Bearer ' + access_token
+        'Authorization': 'Bearer ' + get_access_token()
     }
 
     metadata = {
@@ -71,10 +81,10 @@ def upload_file(access_token, local_path, parent_folder_id):
     return response.json
 
 
-def create_folder(access_token, name, parent_folder_id):
+def create_folder(name, parent_folder_id):
     url = 'https://www.googleapis.com/drive/v3/files?supportsTeamDrives=true'
     headers = {
-        'Authorization': 'Bearer ' + access_token,
+        'Authorization': 'Bearer ' + get_access_token(),
         'Content-Type': 'application/json'
     }
 
@@ -88,7 +98,7 @@ def create_folder(access_token, name, parent_folder_id):
     return response.json()
 
 
-def get_children(access_token, folder_id):
+def get_children(folder_id):
     url_params = {
         'supportsTeamDrives': 'true',
         'includeTeamDriveItems': 'true',
@@ -98,26 +108,24 @@ def get_children(access_token, folder_id):
     }
     url = "https://www.googleapis.com/drive/v3/files?{}".format(urllib.parse.urlencode(url_params))
 
-    print(url)
     headers = {
-        'Authorization': 'Bearer ' + access_token
+        'Authorization': 'Bearer ' + get_access_token()
     }
 
     return requests.get(url, headers=headers).text
 
 
-def search_in_folder(access_token, folder_id, file_name):
-    children = json.loads(get_children(access_token, folder_id))['files']
+def search_in_folder(folder_id, file_name):
+    children = json.loads(get_children(folder_id))['files']
     for child in children:
         if child['name'] == file_name:
             return child['id']
     return None
 
 
-def search_and_create_folder_path(access_token, path):
+def search_and_create_folder_path(path):
     """
     Search and create folders to match given path if needed.
-    :param access_token: token needed to access drive
     :param path: path to be created
     :return: last folder id
     """
@@ -127,23 +135,20 @@ def search_and_create_folder_path(access_token, path):
     for i in range(0, len(path)):
         child_to_search = path[i]
         if search is False:
-            current_folder_id = create_folder(access_token, child_to_search, current_folder_id)['id']
+            current_folder_id = create_folder(child_to_search, current_folder_id)['id']
         else:
-            search_result = search_in_folder(
-                access_token, current_folder_id, child_to_search
-            )
+            search_result = search_in_folder(current_folder_id, child_to_search)
             if search_result is None:
                 search = False
-                current_folder_id = create_folder(access_token, child_to_search, current_folder_id)['id']
+                current_folder_id = create_folder(child_to_search, current_folder_id)['id']
             else:
                 current_folder_id = search_result
     return current_folder_id
 
 
-def search_folder_id(access_token, path):
+def search_folder_id(path):
     """
     Search whether a given path exists, and if it does, return the last folder's id
-    :param access_token: access token needed to access drive
     :param path: file path
     :return: last folder's id
     """
@@ -151,9 +156,7 @@ def search_folder_id(access_token, path):
     current_folder_id = '0AMbf_SZMUMMtUk9PVA'
     for i in range(0, len(path)):
         child_to_search = path[i]
-        search_result = search_in_folder(
-            access_token, current_folder_id, child_to_search
-        )
+        search_result = search_in_folder(current_folder_id, child_to_search)
         if search_result is None:
             return None
         current_folder_id = search_result
@@ -161,12 +164,6 @@ def search_folder_id(access_token, path):
 
 
 if __name__ == '__main__':
-    token = get_access_token()
-    # print(get_child_ids('0AMbf_SZMUMMtUk9PVA', token))
-    # print(download_file('1aPM-E-g-b3P_WXRwiXWwfMOSDiBXLIg0', token))
-    # print(create_folder('testcreatefolder', '1rZFwT0aSX3wko5DYZThmIuPd-9SKlqQA', token))
-    # create_path(token, 'testcreatefolder/001/002/003', '19DJjF2cXvj7ZS8gd2MIKvpp_mcSloKqO')
-    # print(get_children(token, '0AMbf_SZMUMMtUk9PVA'))
-    # print(search_in_folder(token, '0AMbf_SZMUMMtUk9PVA', 'folder800', 'application/vnd.google-apps.folder'))
-    # upload_file(token, 'asdf.txt', search_and_create_folder_path(token, 'testcreatefolder/testcreatefolder/123/789'))
-    print(download_file(token, 'asdf.txt', search_folder_id(token, 'testcreatefolder/testcreatefolder/123/79')))
+    upload_folder_id = search_and_create_folder_path('testcreatefolder/')
+    if upload_folder_id is None:
+        print("No such path")
